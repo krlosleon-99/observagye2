53fbd4cd1c1ca3fc92a0c5dd52af9dd9ed921086
Carlos <contactoatiencia@gmail.com>
2025-05-12 15:13:00 -0500

    puerto

--- config/config.ts
+++ config/config.ts
@@ -1,7 +1,7 @@
 const config = {
     database: {
       host: process.env.HOST_DB, // Dirección del servidor de la base de datos
-      port: 5434,        // Puerto del servidor de la base de datos
+      port: 5432,        // Puerto del servidor de la base de datos
       database: process.env.NAME_DB, // Nombre de la base de datos
       user: process.env.USER_DB,        // Usuario de la base de datos
       password: process.env.PASSWORD_DB,  // Contraseña de la base de datos
